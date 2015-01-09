c59e65fe91a102f9c3dfe1b7aa19ec5ffe714ccd
David McCann <MCCANND@uk.ibm.com>
2015-01-09 10:34:14 +0000

    Add stream to stubproton

--- test/stubs/stubproton.js
+++ test/stubs/stubproton.js
@@ -20,7 +20,11 @@
 /* jshint -W083,-W097 */
 'use strict';
 
-var DEBUG = false;
+var util = require('util');
+var Duplex = require('stream').Duplex;
+
+var DEBUG = process.env.MQLIGHT_NODE_STUB_DEBUG || false;
+var log = process.env.MQLIGHT_NODE_STUB_LOG_ERROR ? console.error : console.log;
 
 var connectStatus = 0;
 
@@ -31,7 +35,7 @@ var connectStatus = 0;
  * @param {string} status Specifies the status to override with.
  */
 exports.setConnectStatus = function(status) {
-  if (DEBUG) console.log('setting connect status to: ' + status);
+  if (DEBUG) log('setting connect status to: ' + status);
   connectStatus = status;
 };
 
@@ -43,7 +47,7 @@ var sendStatus = 7; // PN_STATUS_SETTLED = 7
  * return as PN_STATUS_PENDING.
  */
 exports.blockSendCompletion = function() {
-  if (DEBUG) console.log('blocking send completion');
+  if (DEBUG) log('blocking send completion');
   sendStatus = 1; // PN_STATUS_PENDING = 1
 };
 
@@ -52,7 +56,7 @@ exports.blockSendCompletion = function() {
  * Removes a block on message sends by forcing the status to PN_STATUS_SETTLED.
  */
 exports.unblockSendCompletion = function() {
-  if (DEBUG) console.log('unblocking send completion');
+  if (DEBUG) log('unblocking send completion');
   sendStatus = 7;
 };
 
@@ -69,7 +73,7 @@ var workCallback;
  * @param {function} callback
  */
 exports.setRemoteIdleTimeout = function(interval, callback) {
-  if (DEBUG) console.log('setRemoteIdleTimeout to ' + interval);
+  if (DEBUG) log('setRemoteIdleTimeout to ' + interval);
   remoteIdleTimeout = interval;
   workCallback = callback;
 };
@@ -85,30 +89,29 @@ module.exports.createProtonStub = function() {
   return {
     messenger: {
       send: function() {
-        if (DEBUG) console.log('stub send function called');
+        if (DEBUG) log('stub send function called');
       },
       status: function(msg) {
         var result = sendStatus;
         if (result === 7 && msg.unitTestQos === 0) {
           result = 0;
         }
-        if (DEBUG) console.log('stub status function called, returning: ',
-            result);
+        if (DEBUG) log('stub status function called, returning: ', result);
         return result;
       },
       statusError: function() {
-        if (DEBUG) console.log('stub statusError function called');
+        if (DEBUG) log('stub statusError function called');
         return '';
       },
       accept: function() {
-        if (DEBUG) console.log('stub accept function called');
+        if (DEBUG) log('stub accept function called');
       },
       settle: function() {
-        if (DEBUG) console.log('stub settle function called');
+        if (DEBUG) log('stub settle function called');
       },
       settledCount: 0,
       settled: function() {
-        if (DEBUG) console.log('stub settled function called');
+        if (DEBUG) log('stub settled function called');
         if (connectStatus !== 0) {
           var err = new Error('error on settle: ' + connectStatus);
           err.name = 'NetworkError';
@@ -122,8 +125,8 @@ module.exports.createProtonStub = function() {
         }
       },
       connect: function(service, sslTrustCertificate, sslVerifyName) {
-        if (DEBUG) console.log('stub connect function called for service: ' +
-                               service, sslTrustCertificate, sslVerifyName);
+        if (DEBUG) log('stub connect function called for service: ' +
+                       service, sslTrustCertificate, sslVerifyName);
         if (!this.stopped) throw new Error('already connected');
         var href = service.href;
         var err = null;
@@ -142,19 +145,19 @@ module.exports.createProtonStub = function() {
           } else {
             err = null;
             this.stopped = false;
-            if (DEBUG) console.log('successfully connected');
+            if (DEBUG) log('successfully connected');
           }
         }
         if (err) throw err;
       },
       receive: function() {
         // Commented - as generates a lot of output...
-        // if (DEBUG) console.log('stub receive function called');
+        // if (DEBUG) log('stub receive function called');
         return [];
       },
       stopCount: 2,
       stop: function() {
-        if (DEBUG) console.log('stub stop function called');
+        if (DEBUG) log('stub stop function called');
         if (!this.stopped) {
           this.stopCount--;
           if (this.stopCount === 0) {
@@ -162,25 +165,26 @@ module.exports.createProtonStub = function() {
             this.stopCount = 2;
           }
         }
-        if (DEBUG) console.log('stub stop function returning: '+this.stopped);
+        if (DEBUG) log('stub stop function returning: '+this.stopped);
         return this.stopped;
       },
       put: function(msg, qos) {
-        if (DEBUG) console.log('stub put function called');
+        if (DEBUG) log('stub put function called');
         msg.unitTestQos = qos;
       },
       hasSent: function() {
-        if (DEBUG) console.log('stub hasSent function called');
+        if (DEBUG) log('stub hasSent function called');
         return true;
       },
+      started: true,
       stopped: true,
       subscribe: function() {
-        if (DEBUG) console.log('stub subscribe function called');
+        if (DEBUG) log('stub subscribe function called');
       },
       subscribedCount: 0,
       subscribed: function(address) {
-        if (DEBUG) console.log('stub subscribed function called with address ' +
-                               address);
+        if (DEBUG) log('stub subscribed function called with address ' +
+                       address);
         if (connectStatus !== 0) {
           var err = new Error('error on subscribe: ' + connectStatus);
           err.name = 'NetworkError';
@@ -195,12 +199,12 @@ module.exports.createProtonStub = function() {
         }
       },
       unsubscribe: function() {
-        if (DEBUG) console.log('stub unsubscribe function called');
+        if (DEBUG) log('stub unsubscribe function called');
       },
       unsubscribedCount: 0,
-      unsubscribed: function() {
-        if (DEBUG) console.log('stub unsubscribed function called with ' +
-                               'address ' + address);
+      unsubscribed: function(address) {
+        if (DEBUG) log('stub unsubscribed function called with ' +
+                       'address ' + address);
         if (connectStatus !== 0) {
           var err = new Error('error on unsubscribe: ' + connectStatus);
           err.name = 'NetworkError';
@@ -215,41 +219,109 @@ module.exports.createProtonStub = function() {
         }
       },
       getRemoteIdleTimeout: function(address) {
-        if (DEBUG) console.log('stub getRemoteIdleTimeout function called, ' +
-                               'returning: ' + remoteIdleTimeout);
+        if (DEBUG) log('stub getRemoteIdleTimeout function called, ' +
+                       'returning: ' + remoteIdleTimeout);
         return remoteIdleTimeout;
       },
-      work: function(timeout) {
-        if (DEBUG) console.log('stub work function called with timeout: ' +
-                               timeout);
-        if (workCallback) workCallback.apply();
-        return 0;
-      },
       flow: function(linkAddress, credit) {
-        if (DEBUG) console.log('stub flow function called with link address: ' +
-                               linkAddress + ' and credit: ' + credit);
+        if (DEBUG) log('stub flow function called with link address: ' +
+                       linkAddress + ' and credit: ' + credit);
       },
       pendingOutbound: function(address) {
-        if (DEBUG) console.log('pendingOutbound function called with address ' +
-                               address);
+        if (DEBUG) log('pendingOutbound function called with address ' +
+                       address);
         return false;
+      },
+      push: function(length, chunk) {
+        var result = Math.floor(Math.random() * (length+1));
+        if (DEBUG) log('stub push function called, returning:', result);
+        return result;
+      },
+      pop: function(stream, force) {
+        if (DEBUG) log('stub pop function called');
+        if (force) {
+          if (workCallback) workCallback.apply();
+        }
+        return 0;
+      },
+      closed: function() {
+        if (DEBUG) log('stub closed function called');
+        return 0;
       }
     },
 
     createMessenger: function() {
-      if (DEBUG) console.log('stub createMessenger function called');
+      if (DEBUG) log('stub createMessenger function called');
       connectStatus = 0;
       this.messenger.stopped = true;
       return this.messenger;
     },
 
     createMessage: function() {
-      if (DEBUG) console.log('stub createMessage function called');
+      if (DEBUG) log('stub createMessage function called');
       return {
         destroy: function() {
-          if (DEBUG) console.log('stub destroy function called');
+          if (DEBUG) log('stub destroy function called');
         }
       };
+    },
+
+    connect: function(options, callback) {
+      if (DEBUG) log('stub proton connect function called');
+      process.nextTick(function() {
+        callback.apply(this, []);
+      });
+      return new StubStream();
     }
   };
 };
+
+/**
+ * A stub connection to the Server implemented as a Duplex
+ * Stream.
+ *
+ * @constructor
+ * @param {object} options stream options.
+ * @return {object} a stub connection.
+ */
+
+var StubStream = function(options) {
+  if (DEBUG) log('StubStream constructor called');
+
+  if (!(this instanceof StubStream))
+    return new StubStream(options);
+
+  Duplex.call(this, options);
+
+  this.ended = false;
+  this.authorized = true;
+
+  this._read = function(size) {
+    if (DEBUG) log('stream _read function called for size:', size);
+  };
+
+  this._write = function(chunk, encoding, callback) {
+    if (DEBUG) log('stream _write function called for chunk size:',
+                   chunk.length);
+  };
+
+  this.end = function() {
+    if (DEBUG) log('stream end function called');
+    this.ended = true;
+    this.push(null);
+  };
+
+  var pushData = function(stream) {
+    if (!stream.ended) {
+      stream.push('chunk');
+      setImmediate(function() {
+        pushData(stream);
+      });
+    }
+  };
+
+  setImmediate(function(stream) {
+    pushData(stream)
+  }, this);
+};
+util.inherits(StubStream, Duplex);
