c15f191ba4dbe0c1d273635e2c95a01b40cc2eea
Chris White <Chris.White@uk.ibm.com>
2016-01-05 16:51:32 +0000

    copyright update

--- samples/recv.js
+++ samples/recv.js
@@ -3,13 +3,13 @@
  * <copyright
  * notice="lm-source-program"
  * pids="5725-P60"
- * years="2013,2014"
+ * years="2013,2016"
  * crc="3568777996" >
  * Licensed Materials - Property of IBM
  *
  * 5725-P60
  *
- * (C) Copyright IBM Corp. 2013, 2014
+ * (C) Copyright IBM Corp. 2013, 2016
  *
  * US Government Users Restricted Rights - Use, duplication or
  * disclosure restricted by GSA ADP Schedule Contract with
